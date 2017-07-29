970475f20ae9aa50830c8d9f2be7ac56539271e7
Łukasz Jagliński <ljaglinski@o2.pl>
2017-07-29 13:09:13 +0200

    Initial commit

---
+++ src/main/java/pl/fitness/FitnessWebApp/FitnessWebAppApplication.java
@@ -0,0 +1,12 @@
+package pl.fitness.FitnessWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessWebAppApplication.class, args);
+	}
+}
